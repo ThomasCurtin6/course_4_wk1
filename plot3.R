f3b1fa4f8e2bb9ca1adf4804af6ac56a8469b344
Thomas Curtin <roadweaver6@gmail.com>
2017-02-17 04:16:13 -0600

    reviewed ranges for plots 3&4.. they look more like they should be 35

--- plot3.R
+++ plot3.R
@@ -5,10 +5,10 @@ source("getdata.r")
 dev.off(dev.list()["RStudioGD"])
 plot.new()
 
-with(mydata,plot(rdate2, as.numeric(as.character(Sub_metering_1)), type="l", xlab="", ylab="Energy sub metering", ylim=c(0,30)))
+with(mydata,plot(rdate2, as.numeric(as.character(Sub_metering_1)), type="l", xlab="", ylab="Energy sub metering", ylim=c(0,35)))
 with(mydata,lines(rdate2, as.numeric(as.character(Sub_metering_2)), col="red"))
 with(mydata,lines(rdate2, as.numeric(as.character(Sub_metering_3)), col="blue"))
-legend("topright", lty=1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" )) 
+legend("topright",  col = c("black", "red", "blue"),lty=1, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" )) 
 
 dev.copy(png, file="plot3.png", height=480, width=480) 
 dev.off() 
--- plot4.R
+++ plot4.R
@@ -7,15 +7,15 @@ par(mfrow=c(2,2))
 title(main="Plot 4",col=blue)
 with(mydata,plot(rdate2, as.numeric(as.character(Global_active_power)), type="l", xlab="", ylab="Global Active Power"))
 
-with(mydata,plot(rdate2, as.numeric(as.character(Voltage)), type="l", xlab="", ylab="Voltage"))
+with(mydata,plot(rdate2, as.numeric(as.character(Voltage)), type="l", xlab="datetime", ylab="Voltage"))
 
-with(mydata,plot (rdate2, as.numeric(as.character(Sub_metering_1)), type="l", xlab="", ylab="Energy sub metering", ylim=c(0,40)))
-with(mydata,lines(rdate2, as.numeric(as.character(Sub_metering_2)), col="red"))
+with(mydata,plot (rdate2, as.numeric(as.character(Sub_metering_1)), type="l", xlab="", ylab="Energy sub metering", ylim=c(0,35)))
 with(mydata,lines(rdate2, as.numeric(as.character(Sub_metering_3)), col="blue") )
+with(mydata,lines(rdate2, as.numeric(as.character(Sub_metering_2)), col="red"))
 
-legend("topright", lty=1, bty="n", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" )) 
+legend("topright", lty=1,  col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" )) 
 
-with(mydata,plot(rdate2, as.numeric(as.character(Global_reactive_power)), type="l", xlab="", ylab="Global_reactive_power"))
+with(mydata,plot(rdate2, as.numeric(as.character(Global_reactive_power)), type="l", xlab="datetime", ylab="Global_reactive_power"))
 
 dev.copy(png, file="plot4.png", height=480, width=480) 
 dev.off() 
